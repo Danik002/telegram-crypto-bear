f1f1afc5c61e7bf7fabfafb32b60057d33939147
danik safon <66772628+Danik002@users.noreply.github.com>
2025-04-28 13:44:09 +0300

    Update bot.py

---
+++ bot.py
@@ -0,0 +1,12 @@
+from aiogram import Bot, Dispatcher, executor, types
+import os
+
+bot = Bot(token=os.getenv("BOT_TOKEN"))  # Токен берется из переменных окружения
+dp = Dispatcher(bot)
+
+@dp.message_handler(commands=['start'])
+async def start(message: types.Message):
+    await message.answer("Привет! Я крипто-бот! 🐻")
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
